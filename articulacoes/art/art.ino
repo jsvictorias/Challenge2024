d79ea39204b5930e3a6aec3b9b3115d469159cd7
jsvictorias <juliasantos7652@gmail.com>
2024-06-12 13:14:47 -0300

    codigo que supostamente ajusta os potenciometros com os servos

--- articulacoes/art/art.ino
+++ articulacoes/art/art.ino
@@ -21,6 +21,16 @@ Servo servo3;
 Servo servo4;
 Servo servo5;
 
+// Variáveis para armazenar os ângulos atuais dos servos
+int angle1 = 0;
+int angle2 = 0;
+int angle3 = 0;
+int angle4 = 0;
+int angle5 = 0;
+
+// Variável para armazenar o valor de mudança significativa
+const int threshold = 5;
+
 void setup() {
   // Conecta os servos aos pinos digitais
   servo1.attach(SERVO1_PIN);
@@ -28,6 +38,13 @@ void setup() {
   servo3.attach(SERVO3_PIN);
   servo4.attach(SERVO4_PIN);
   servo5.attach(SERVO5_PIN);
+
+  // Move os servos para a posição inicial
+  servo1.write(angle1);
+  servo2.write(angle2);
+  servo3.write(angle3);
+  servo4.write(angle4);
+  servo5.write(angle5);
 }
 
 void loop() {
@@ -39,18 +56,37 @@ void loop() {
   int pot5Val = analogRead(POT5_PIN);
 
   // Mapeia os valores dos potenciômetros para ângulos de 0 a 180
-  int angle1 = map(pot1Val, 0, 1023, 0, 180);
-  int angle2 = map(pot2Val, 0, 1023, 0, 180);
-  int angle3 = map(pot3Val, 0, 1023, 0, 180);
-  int angle4 = map(pot4Val, 0, 1023, 0, 180);
-  int angle5 = map(pot5Val, 0, 1023, 0, 180);
+  int newAngle1 = map(pot1Val, 0, 1023, 0, 180);
+  int newAngle2 = map(pot2Val, 0, 1023, 0, 180);
+  int newAngle3 = map(pot3Val, 0, 1023, 0, 180);
+  int newAngle4 = map(pot4Val, 0, 1023, 0, 180);
+  int newAngle5 = map(pot5Val, 0, 1023, 0, 180);
 
-  // Move os servos para os ângulos correspondentes
-  servo1.write(angle1);
-  servo2.write(angle2);
-  servo3.write(angle3);
-  servo4.write(angle4);
-  servo5.write(angle5);
+  // Verifica se a mudança é significativa para mover os servos
+  if (abs(newAngle1 - angle1) > threshold) {
+    angle1 = newAngle1;
+    servo1.write(angle1);
+  }
+
+  if (abs(newAngle2 - angle2) > threshold) {
+    angle2 = newAngle2;
+    servo2.write(angle2);
+  }
+
+  if (abs(newAngle3 - angle3) > threshold) {
+    angle3 = newAngle3;
+    servo3.write(angle3);
+  }
+
+  if (abs(newAngle4 - angle4) > threshold) {
+    angle4 = newAngle4;
+    servo4.write(angle4);
+  }
+
+  if (abs(newAngle5 - angle5) > threshold) {
+    angle5 = newAngle5;
+    servo5.write(angle5);
+  }
 
   // Pequeno atraso para suavizar o movimento
   delay(15);
